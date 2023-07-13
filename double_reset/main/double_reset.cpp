627e713a872ca08c1ef85fde05d8cc878ad06a8e
buggsboony <boonydeboon@gmail.com>
2023-07-13 00:24:42 -0400

    nvs storage works

--- double_reset/main/double_reset.cpp
+++ double_reset/main/double_reset.cpp
@@ -3,7 +3,15 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+
+using namespace std;
+
 static const char *TAG = "MyTask";
+
+TaskHandle_t mainTaskHandle;
+nvs_handle_t nvsHandle;
 
 // Fonction de la tâche
 void task(void *pvParameters)
@@ -16,7 +24,6 @@ void task(void *pvParameters)
 
     // Fin de la tâche
     printf("Tâche terminée.\n");
-
 
     // Supprimer la tâche
     vTaskDelete(NULL);
@@ -31,7 +38,7 @@ void mainJob(void *pvParameters)
     // Tâche en cours d'exécution
     printf("mainJob started\n");
 
-    for(int i=0; i<14; i++)
+    for(int i=0; i<9; i++)
     {
         ESP_LOGI(TAG,">%d",i);
         // Simuler un travail
@@ -41,24 +48,94 @@ void mainJob(void *pvParameters)
     printf("Job ended.\n");
 
     // Send notification to waiting task
-    xTaskNotifyGive(0);
+    xTaskNotifyGive(mainTaskHandle);
 
     // Supprimer la tâche
     vTaskDelete(NULL);  
 }//mainJob
 
-extern "C" void app_main(void)
+
+
+
+
+////2023-07-13 00:23:40 - Usage : 
+// nvs_flash_init();
+// nvs_open("storage", NVS_READWRITE, &nvsHandle);
+
+// //2023-07-13 00:22:14 - Write
+// nvs_write_string(nvsHandle,"reset_state","OFF");
+
+// //2023-07-13 00:22:20 - read
+// string state = nvs_read_string(nvsHandle,"reset_state"); 
+// cout<<"Read from func : "<<state<<endl;
+
+
+
+
+//2023-07-12 23:24:59 - Write to NVS
+void nvs_write_string(nvs_handle_t h,string key, string write_string)
 {
+   nvs_set_str(h, key.c_str(), write_string.c_str() );
+   nvs_commit(h);
+}
+
+//2023-07-12 23:24:51 - Read From NVS
+string nvs_read_string(nvs_handle_t h, string key)
+{
+   size_t required_size = 0;
+   nvs_get_str(h,key.c_str(), NULL, &required_size);
+   cout<<"size to read : "<< required_size<<endl;
+   printf("req size = %d\n", required_size);
+   if( required_size<=0) return "";
+   char *buff = (char*) malloc(required_size);
+   nvs_get_str(h, key.c_str(), buff, &required_size);
+   return buff;
+}
+
+  
+
+
+
+
+
+extern "C" void app_main(void)
+{    
     // Initialize logging
     esp_log_level_set(TAG, ESP_LOG_INFO);  // Set log level to INFO
+  mainTaskHandle = xTaskGetCurrentTaskHandle();
+
+
+nvs_flash_init();
+nvs_open("storage", NVS_READWRITE, &nvsHandle);
+
+//2023-07-13 00:22:14 - Write
+nvs_write_string(nvsHandle,"reset_state","OFF");
+
+//2023-07-13 00:22:20 - read
+string state = nvs_read_string(nvsHandle,"reset_state"); 
+cout<<"Read from func : "<<state<<endl;
+
+
+
+
+
 
       // Créer la tâche
-    xTaskCreate(task, "myTask", 2048, NULL, 5, NULL);
-    xTaskCreate(mainJob, "mainJob", 2048, NULL, 5, &jobHandle);
+//     xTaskCreate(task, "myTask", 2048, NULL, 5, NULL);
+//     xTaskCreate(mainJob, "mainJob", 2048, NULL, 5, &jobHandle);
 
-    // Attendre la fin de la tâche
-   puts("Wait for job task to finish\n");
-   xTaskNotifyWait(0, 0, NULL, portMAX_DELAY);
-    //vTaskDelete(NULL);    
-    puts("End main!\n");
+
+
+
+
+
+
+
+
+
+//     // Attendre la fin de la tâche
+//    puts("Wait for job task to finish\n");
+//    xTaskNotifyWait(0, 0, NULL, portMAX_DELAY);
+//     //vTaskDelete(NULL);    
+//     puts("End main!\n");
 }
